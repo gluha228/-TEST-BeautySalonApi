a8ea3eba3981f4caf9fb77fd499dee2486591735
Vitalie <vitaliesandiuc@gmail.com>
2022-07-13 19:57:58 +0300

    g

--- src/main/java/com/powerit/beautysalonapi/BeautySalonApiApplication.java
+++ src/main/java/com/powerit/beautysalonapi/BeautySalonApiApplication.java
@@ -2,12 +2,15 @@ package com.powerit.beautysalonapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.SpringVersion;
 
 @SpringBootApplication
 public class BeautySalonApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BeautySalonApiApplication.class, args);
+
+        System.out.println("version: " + SpringVersion.getVersion());
     }
 
 }
